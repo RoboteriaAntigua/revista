687306e2f9603a0ba7d25baa738fe60ac56c48b7
marcos ricciardelli <marcos.ricciardelli@alliansys.com.ar>
2024-05-16 10:47:00 -0300

    with lazyload

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 var zlib = require('zlib'); // Import zlib for gzip compression
 
 router.get('/', (req, res) => {
-    res.render('index');
+    res.render('index', { lazyload: 'ok' });      //lazyload es para lazy load
 })
 
 router.get('/index', (req, res) => {
@@ -11,11 +11,11 @@ router.get('/index', (req, res) => {
 })
 
 router.get('/about', (req, res) => {
-    res.render('about.ejs', { enconstruccion: '' });
+    res.render('about.ejs', { enconstruccion: '', lazyload: 'ok' });
 })
 
 router.get('/post', (req, res) => {
-    res.render('post.ejs');
+    res.render('post.ejs', { lazyload: 'ok' });
 })
 
 //Sitemap
@@ -23,68 +23,71 @@ router.get('/sitemap', (req, res) => {
     res.sendFile(__dirname + '/sitemap.txt');
 })
 
+router.get('/contact', (req, res) => {
+    res.render('contact', { lazyload: 'ok' });      //currentPage es para lazy load
+})
 
 /********************** Post interesantes ******************************************************************* */
-router.get('/energia-centro-tierra', (req, res) => { res.render('articulos/energia-centro-tierra'); })
-router.get('/dinamitaypaz', (req, res) => { res.render('articulos/dinamitaypaz'); })
-router.get('/laplacedemonio', (req, res) => { res.render('articulos/ellibrodelosacontecimientos'); })
-router.get('/pucaras', (req, res) => { res.render('articulos/pucara-ñandus'); })
+router.get('/energia-centro-tierra', (req, res) => { res.render('articulos/energia-centro-tierra', { lazyload: 'ok' }); })
+router.get('/dinamitaypaz', (req, res) => { res.render('articulos/dinamitaypaz',{ lazyload: 'ok' }); })
+router.get('/laplacedemonio', (req, res) => { res.render('articulos/ellibrodelosacontecimientos', { lazyload: 'ok' }); })
+router.get('/pucaras', (req, res) => { res.render('articulos/pucara-ñandus', { lazyload: 'ok' }); })
 
 /********************** Fin de Post interesantes ******************************************************************* */
 
 
 /********************** Varios ******************************************************************* */
 router.get('/buenas-practicas-js', (req, res) => {
-    res.render('cursos/buenas-practicas-js');
+    res.render('cursos/buenas-practicas-js',{ lazyload: 'ok' });
 })
 /********************** Finde varios ******************************************************************* */
 
 /******************************** curso de mysql **************************************/
-router.get('/mysql-introduccion', (req, res) => { res.render('cursos/mysql/mysql-introduccion'); })
-router.get('/mysql-instalacion', (req, res) => { res.render('cursos/mysql/mysql-instalacion'); })
-router.get('/mysql-constructivas', (req, res) => { res.render('cursos/mysql/mysql-constructivas'); })
-router.get('/mysql-seleccion', (req, res) => { res.render('cursos/mysql/mysql-seleccion'); })
-router.get('/mysql-destructivas', (req, res) => { res.render('cursos/mysql/mysql-destructivas'); })
-router.get('/mysql-dba', (req, res) => { res.render('cursos/mysql/mysql-dba'); })
-router.get('/mysql-backup', (req, res) => { res.render('cursos/mysql/mysql-backups'); })
-router.get('/mysql-views', (req, res) => { res.render('cursos/mysql/mysql-views'); })
-router.get('/mysql-abm', (req, res) => { res.render('cursos/mysql/mysql-abm'); })
-router.get('/mysql-relacion-1a1', (req, res) => { res.render('about.ejs', { enconstruccion: 'Pagina en construccion' }); })
-router.get('/mysql-relacion-1aM', (req, res) => { res.render('about.ejs', { enconstruccion: 'Pagina en construccion' }); })
-router.get('/mysql-relacion-MaM', (req, res) => { res.render('about.ejs', { enconstruccion: 'Pagina en construccion' }); })
+router.get('/mysql-introduccion', (req, res) => { res.render('cursos/mysql/mysql-introduccion',{ lazyload: 'ok' }); })
+router.get('/mysql-instalacion', (req, res) => { res.render('cursos/mysql/mysql-instalacion',{ lazyload: 'ok' }); })
+router.get('/mysql-constructivas', (req, res) => { res.render('cursos/mysql/mysql-constructivas',{ lazyload: 'ok' }); })
+router.get('/mysql-seleccion', (req, res) => { res.render('cursos/mysql/mysql-seleccion',{ lazyload: 'ok' }); })
+router.get('/mysql-destructivas', (req, res) => { res.render('cursos/mysql/mysql-destructivas',{ lazyload: 'ok' }); })
+router.get('/mysql-dba', (req, res) => { res.render('cursos/mysql/mysql-dba',{ lazyload: 'ok' }); })
+router.get('/mysql-backup', (req, res) => { res.render('cursos/mysql/mysql-backups',{ lazyload: 'ok' }); })
+router.get('/mysql-views', (req, res) => { res.render('cursos/mysql/mysql-views',{ lazyload: 'ok' }); })
+router.get('/mysql-abm', (req, res) => { res.render('cursos/mysql/mysql-abm',{ lazyload: 'ok' }); })
+router.get('/mysql-relacion-1a1', (req, res) => { res.render('about.ejs', { enconstruccion: 'Pagina en construccion' ,lazyload: 'ok'}); })
+router.get('/mysql-relacion-1aM', (req, res) => { res.render('about.ejs', { enconstruccion: 'Pagina en construccion' ,lazyload: 'ok'}); })
+router.get('/mysql-relacion-MaM', (req, res) => { res.render('about.ejs', { enconstruccion: 'Pagina en construccion' ,lazyload: 'ok'}); })
 
 /*******************************fin de curso de mysql **************************************/
 
 /******************************** curso de kubernetes **************************************/
-router.get('/kubernetes-introduccion', (req, res) => { res.render('cursos/kubernetes/kub-introduccion.ejs'); });
-router.get('/kubernetes-conceptos', (req, res) => { res.render('cursos/kubernetes/kub-conceptos.ejs'); });
-router.get('/kubernetes-minikube', (req, res) => { res.render('cursos/kubernetes/kub-minikube.ejs'); });
-router.get('/kubernetes-services', (req, res) => { res.render('cursos/kubernetes/kub-services.ejs'); });
-router.get('/kubernetes-exponer-app-node', (req, res) => { res.render('cursos/kubernetes/kub-deploy-node.ejs'); });
-router.get('/kubernetes-solucionar-problemas', (req, res) => { res.render('cursos/kubernetes/kub-solucionar.ejs'); });
-router.get('/kubernetes-labels', (req, res) => { res.render('cursos/kubernetes/kub-alias.ejs'); });
-router.get('/kubernetes-multiples-instancias', (req, res) => { res.render('cursos/kubernetes/kub-multiples-instancias.ejs'); });
-router.get('/kubernetes-updates-no-down', (req, res) => { res.render('cursos/kubernetes/kub-updates.ejs'); });
+router.get('/kubernetes-introduccion', (req, res) => { res.render('cursos/kubernetes/kub-introduccion.ejs',{ lazyload: 'ok' }); });
+router.get('/kubernetes-conceptos', (req, res) => { res.render('cursos/kubernetes/kub-conceptos.ejs',{ lazyload: 'ok' }); });
+router.get('/kubernetes-minikube', (req, res) => { res.render('cursos/kubernetes/kub-minikube.ejs',{ lazyload: 'ok' }); });
+router.get('/kubernetes-services', (req, res) => { res.render('cursos/kubernetes/kub-services.ejs',{ lazyload: 'ok' }); });
+router.get('/kubernetes-exponer-app-node', (req, res) => { res.render('cursos/kubernetes/kub-deploy-node.ejs',{ lazyload: 'ok' }); });
+router.get('/kubernetes-solucionar-problemas', (req, res) => { res.render('cursos/kubernetes/kub-solucionar.ejs',{ lazyload: 'ok' }); });
+router.get('/kubernetes-labels', (req, res) => { res.render('cursos/kubernetes/kub-alias.ejs',{ lazyload: 'ok' }); });
+router.get('/kubernetes-multiples-instancias', (req, res) => { res.render('cursos/kubernetes/kub-multiples-instancias.ejs',{ lazyload: 'ok' }); });
+router.get('/kubernetes-updates-no-down', (req, res) => { res.render('cursos/kubernetes/kub-updates.ejs',{ lazyload: 'ok' }); });
 
 /******************************** fin curso de kubernetes **************************************/
 
 /**********************************Angular ****************************************************/
-router.get('/inyeccion', (req, res) => { res.render('cursos/angular/inyeccion.ejs'); });
-router.get('/servicios', (req, res) => { res.render('cursos/angular/servicios.ejs'); });
-router.get('/observer-patron', (req, res) => { res.render('cursos/angular/observer-patron.ejs'); });
-router.get('/observables-angular', (req, res) => { res.render('cursos/angular/obs-angular.ejs'); });
-router.get('/HttpClient', (req, res) => { res.render('cursos/angular/http.ejs'); });
-router.get('/guards', (req, res) => { res.render('cursos/angular/guards.ejs'); });
-router.get('/login-angular', (req, res) => { res.render('cursos/angular/login-angular.ejs'); });
-router.get('/interceptors', (req, res) => { res.render('cursos/angular/interceptors.ejs'); });
-router.get('/flux', (req, res) => { res.render('cursos/angular/flux.ejs'); });
-router.get('/flux-ejemplo', (req, res) => { res.render('cursos/angular/flux-ejemplo.ejs'); });
+router.get('/inyeccion', (req, res) => { res.render('cursos/angular/inyeccion.ejs',{ lazyload: 'ok' }); });
+router.get('/servicios', (req, res) => { res.render('cursos/angular/servicios.ejs',{ lazyload: 'ok' }); });
+router.get('/observer-patron', (req, res) => { res.render('cursos/angular/observer-patron.ejs',{ lazyload: 'ok' }); });
+router.get('/observables-angular', (req, res) => { res.render('cursos/angular/obs-angular.ejs',{ lazyload: 'ok' }); });
+router.get('/HttpClient', (req, res) => { res.render('cursos/angular/http.ejs',{ lazyload: 'ok' }); });
+router.get('/guards', (req, res) => { res.render('cursos/angular/guards.ejs',{ lazyload: 'ok' }); });
+router.get('/login-angular', (req, res) => { res.render('cursos/angular/login-angular.ejs',{ lazyload: 'ok' }); });
+router.get('/interceptors', (req, res) => { res.render('cursos/angular/interceptors.ejs',{ lazyload: 'ok' }); });
+router.get('/flux', (req, res) => { res.render('cursos/angular/flux.ejs',{ lazyload: 'ok' }); });
+router.get('/flux-ejemplo', (req, res) => { res.render('cursos/angular/flux-ejemplo.ejs',{ lazyload: 'ok' }); });
 
 /******************************************* fin de angular avanzado */
 
 /**********************************Networking ****************************************************/
-router.get('/iptables', (req, res) => { res.render('cursos/networking/iptables.ejs'); });
-router.get('/icmp', (req, res) => { res.render('cursos/networking/icmp.ejs'); });
+router.get('/iptables', (req, res) => { res.render('cursos/networking/iptables.ejs',{ lazyload: 'ok' }); });
+router.get('/icmp', (req, res) => { res.render('cursos/networking/icmp.ejs',{ lazyload: 'ok' }); });
 
 
 /**********************************Fin Networking ****************************************************/
